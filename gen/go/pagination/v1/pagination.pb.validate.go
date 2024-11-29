4b97cbe08e120c8da818d9dc7de72dea31c81838
godcong <jumbycc@163.com>
2024-11-29 18:13:29 +0800

    refactor(pagination): update proto and go files for consistency

--- gen/go/pagination/v1/pagination.pb.validate.go
+++ gen/go/pagination/v1/pagination.pb.validate.go
@@ -61,12 +61,14 @@ func (m *PageRequest) validate(all bool) error {
 
 	// no validation rules for OnlyCount
 
+	// no validation rules for OrderBy
+
 	if all {
-		switch v := interface{}(m.GetFieldMask()).(type) {
+		switch v := interface{}(m.GetUpdateMask()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
 				errors = append(errors, PageRequestValidationError{
-					field:  "FieldMask",
+					field:  "UpdateMask",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
@@ -74,16 +76,16 @@ func (m *PageRequest) validate(all bool) error {
 		case interface{ Validate() error }:
 			if err := v.Validate(); err != nil {
 				errors = append(errors, PageRequestValidationError{
-					field:  "FieldMask",
+					field:  "UpdateMask",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
 			}
 		}
-	} else if v, ok := interface{}(m.GetFieldMask()).(interface{ Validate() error }); ok {
+	} else if v, ok := interface{}(m.GetUpdateMask()).(interface{ Validate() error }); ok {
 		if err := v.Validate(); err != nil {
 			return PageRequestValidationError{
-				field:  "FieldMask",
+				field:  "UpdateMask",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
