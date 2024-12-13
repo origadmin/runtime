26b31b58dbf66f9da4f9764b8ca208a26125ae08
godcong <jumbycc@163.com>
2024-12-13 18:53:48 +0800

    test(runtime/fileupload): update expected hash value in builder_test.go
    - Change the expected hash value from '0d407ee6...' to '9f86d081...' in a test case
    - This update ensures the test matches the correct hash for the given input

--- fileupload/builder_test.go
+++ fileupload/builder_test.go
@@ -43,7 +43,7 @@ func TestGenerateFileNameHash(t *testing.T) {
 			args: args{
 				name: "test.jpg",
 			},
-			want: "0d407ee6406a1216f2366674a1a9ff71361d5bef47021f8eb8b51f95e319dd56.jpg",
+			want: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08.jpg",
 		},
 	}
 	for _, tt := range tests {
