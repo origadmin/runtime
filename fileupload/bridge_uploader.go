193e7aa44a8fc6ce496e847d1b967c51d5c8d375
godcong <jumbycc@163.com>
2025-04-28 04:34:42 +0800

    feat(fileupload): implement gRPC upload functionality and refactor HTTP uploader
    - Add gRPC upload functionality to both client and server
    - Refactor HTTP uploader to use chunked uploading and buffer pools
    - Create README.md with detailed feature list and implementation plan
    - Add service_type.go and service_type_string.go for service type enumeration

--- fileupload/bridge_uploader.go
+++ fileupload/bridge_uploader.go
@@ -24,26 +24,20 @@ func NewBridgeUploader(builder *Builder, service fileupload.Uploader) fileupload
 func (b *BridgeUploader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	// 1. Create an HTTP receiver to receive uploaded files
 	httpReceiver := b.Builder.NewReceiver(r, w)
-
-	// 2. Obtain the file fileHeader information
 	header, err := httpReceiver.GetFileHeader(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// 3. Create a gRPC uploader
 	grpcUploader := b.Service
 
-	// 4. Set the file fileHeader information
 	if err := grpcUploader.SetFileHeader(ctx, header); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// 5. Get the contents of the uploaded file
 	reader, err := httpReceiver.ReceiveFile(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -51,23 +45,19 @@ func (b *BridgeUploader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	defer reader.Close()
 
-	// 6. Upload the file to the gRPC service
 	if err := grpcUploader.UploadFile(ctx, reader); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// 7. Complete the upload and get the response
 	resp, err := grpcUploader.Finalize(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// 8. Forward the gRPC response back to the HTTP client
 	errFin := httpReceiver.Finalize(ctx, resp)
 	if errFin != nil {
 		http.Error(w, errFin.Error(), http.StatusInternalServerError)
-		return
 	}
 }
---
+++ fileupload/go.mod
@@ -0,0 +1,37 @@
+module github.com/origadmin/runtime/fileupload
+
+go 1.23.1
+
+require (
+	github.com/envoyproxy/protoc-gen-validate v1.2.1
+	github.com/go-kratos/kratos/v2 v2.8.4
+	github.com/google/gnostic v0.7.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20250425173222-7b384671a197
+	google.golang.org/grpc v1.72.0
+	google.golang.org/protobuf v1.36.6
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/go-kratos/aegis v0.2.0 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250414145226-207652e42e2e // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+retract (
+	v0.0.90
+	v0.0.89
+	v0.0.88
+	v0.0.87
+)
