05b4394c44a8ffaf91046fd506cc273043f22e03
godcong <jumbycc@163.com>
2025-11-19 18:03:37 +0800

    feat(runtime): remove unused registry interfaces and rename empty middleware to noop

--- middleware/noop.go
+++ middleware/noop.go
@@ -1,8 +1,8 @@
 // Package middleware implements the functions, types, and interfaces for the module.
 package middleware
 
-// Empty returns an empty middleware.
-func Empty() KMiddleware {
+// Noop returns an empty middleware.
+func Noop() KMiddleware {
 	return func(handler KHandler) KHandler {
 		return handler
 	}
