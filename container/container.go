882ee489036542ba9b8c289f9b83064f7c887d89
godcong <jumbycc@163.com>
2025-11-22 02:48:25 +0800

    feat(container): refactor component initialization with options support and add Logger accessor

--- container/container.go
+++ container/container.go
@@ -25,16 +25,18 @@ type Container interface {
 	Cache(opts ...options.Option) (CacheProvider, error)
 	Database(opts ...options.Option) (DatabaseProvider, error)
 	ObjectStore(opts ...options.Option) (ObjectStoreProvider, error)
-
 	// Components returns all initialized components.
 	// The first call will trigger the initialization process.
-	Components() (map[string]interfaces.Component, error)
+	Components(opts ...options.Option) (map[string]interfaces.Component, error)
 	// Component returns a specific component by name.
 	// The first call will trigger the initialization process.
 	Component(name string) (interfaces.Component, error)
 	// RegisterComponent allows for dynamic, programmatic registration of components.
 	// This is the primary mechanism for factories to add created components to the container.
 	RegisterComponent(name string, comp interfaces.Component)
+
+	// Logger returns the configured logger instance.
+	Logger() log.Logger
 }
 
 // containerImpl implements the Container interface with lazy loading and caching.
@@ -59,28 +61,18 @@ type containerImpl struct {
 }
 
 // NewContainer creates a new Container instance with the given configuration.
-func NewContainer(config interfaces.StructuredConfig, componentFactories map[string]ComponentFactory, opts ...options.Option) Container {
-	// Instantiate Logger internally
+func NewContainer(config interfaces.StructuredConfig, opts ...options.Option) Container {
+	componentFactories := ComponentFactoryFromOptions(opts...)
+	var logger log.Logger
 	loggerConfig, err := config.DecodeLogger()
 	if err != nil {
 		// Log the error using a default logger, then proceed with default logger
 		log.DefaultLogger.Log(log.LevelError, "failed to decode logger config, using default logger: %v", err)
-		logger := log.DefaultLogger
-		return &containerImpl{
-			config:              config,
-			logger:              logger,
-			opts:                opts,
-			componentFactories:  componentFactories,
-			cachedComponents:    make(map[string]interfaces.Component),
-			middlewareProvider:  middleware.NewProvider(logger),
-			cacheProvider:       cache.NewProvider(logger),
-			databaseProvider:    database.NewProvider(logger),
-			registryProvider:    registry.NewProvider(logger),
-			objectStoreProvider: objectstore.NewProvider(logger),
-		}
+		// Set loggerConfig to nil so runtimelog.NewLogger returns DefaultLogger
+		loggerConfig = nil
 	}
 
-	logger := runtimelog.NewLogger(loggerConfig)
+	logger = runtimelog.NewLogger(loggerConfig)
 
 	impl := &containerImpl{
 		config:              config,
@@ -99,7 +91,7 @@ func NewContainer(config interfaces.StructuredConfig, componentFactories map[str
 }
 
 // initializeComponents is the core logic for component initialization.
-func (c *containerImpl) initializeComponents() {
+func (c *containerImpl) initializeComponents(opts ...options.Option) {
 	logHelper := log.NewHelper(c.logger)
 
 	factories := make([]ComponentFactory, 0, len(c.componentFactories))
@@ -111,9 +103,12 @@ func (c *containerImpl) initializeComponents() {
 		return factories[i].Priority() < factories[j].Priority()
 	})
 
+	// Merge static opts from NewContainer and dynamic opts from Components() call
+	finalOpts := append(append([]options.Option{}, c.opts...), opts...)
+
 	for _, factory := range factories {
 		logHelper.Infof("executing component factory with priority %d", factory.Priority())
-		if _, err := factory.NewComponent(c.config, c, c.opts...); err != nil {
+		if _, err := factory.NewComponent(c.config, c, finalOpts...); err != nil {
 			c.initErr = fmt.Errorf("error executing factory with priority %d: %w", factory.Priority(), err)
 			logHelper.Errorf("halting component initialization due to error: %v", c.initErr)
 			return
@@ -122,8 +117,10 @@ func (c *containerImpl) initializeComponents() {
 }
 
 // Components returns all initialized components.
-func (c *containerImpl) Components() (map[string]interfaces.Component, error) {
-	c.onceComponents.Do(c.initializeComponents)
+func (c *containerImpl) Components(opts ...options.Option) (map[string]interfaces.Component, error) {
+	c.onceComponents.Do(func() {
+		c.initializeComponents(opts...) // Pass opts to the actual initialization logic
+	})
 	if c.initErr != nil {
 		return nil, c.initErr
 	}
@@ -139,9 +136,9 @@ func (c *containerImpl) Components() (map[string]interfaces.Component, error) {
 
 // Component returns a specific component by name.
 func (c *containerImpl) Component(name string) (interfaces.Component, error) {
-	c.onceComponents.Do(c.initializeComponents)
-	if c.initErr != nil {
-		return nil, c.initErr
+	// Component() call will trigger initializeComponents() if not already done
+	if _, err := c.Components(); err != nil {
+		return nil, err
 	}
 
 	c.componentMu.RLock()
@@ -164,9 +161,16 @@ func (c *containerImpl) RegisterComponent(name string, comp interfaces.Component
 	c.cachedComponents[name] = comp
 }
 
+// Logger returns the configured logger instance.
+func (c *containerImpl) Logger() log.Logger {
+	return c.logger
+}
+
 // Registry implements Container.
 func (c *containerImpl) Registry(opts ...options.Option) (RegistryProvider, error) {
-	c.onceComponents.Do(c.initializeComponents)
+	c.onceComponents.Do(func() {
+		c.initializeComponents()
+	})
 	if c.initErr != nil {
 		return nil, c.initErr
 	}
@@ -174,8 +178,6 @@ func (c *containerImpl) Registry(opts ...options.Option) (RegistryProvider, erro
 	if err != nil {
 		return nil, err
 	}
-	// Here we assume SetConfig will be adapted to handle options.
-	// For now, we merge and pass them, though the provider might not use them yet.
 	finalOpts := append(append([]options.Option{}, c.opts...), opts...)
 	c.registryProvider.SetConfig(discoveries, finalOpts...)
 	return c.registryProvider, nil
@@ -183,7 +185,9 @@ func (c *containerImpl) Registry(opts ...options.Option) (RegistryProvider, erro
 
 // Middleware implements Container.
 func (c *containerImpl) Middleware(opts ...options.Option) (MiddlewareProvider, error) {
-	c.onceComponents.Do(c.initializeComponents)
+	c.onceComponents.Do(func() {
+		c.initializeComponents()
+	})
 	if c.initErr != nil {
 		return nil, c.initErr
 	}
@@ -198,7 +202,9 @@ func (c *containerImpl) Middleware(opts ...options.Option) (MiddlewareProvider, 
 
 // Cache implements Container.
 func (c *containerImpl) Cache(opts ...options.Option) (CacheProvider, error) {
-	c.onceComponents.Do(c.initializeComponents)
+	c.onceComponents.Do(func() {
+		c.initializeComponents()
+	})
 	if c.initErr != nil {
 		return nil, c.initErr
 	}
@@ -213,7 +219,9 @@ func (c *containerImpl) Cache(opts ...options.Option) (CacheProvider, error) {
 
 // Database implements Container.
 func (c *containerImpl) Database(opts ...options.Option) (DatabaseProvider, error) {
-	c.onceComponents.Do(c.initializeComponents)
+	c.onceComponents.Do(func() {
+		c.initializeComponents()
+	})
 	if c.initErr != nil {
 		return nil, c.initErr
 	}
@@ -228,7 +236,9 @@ func (c *containerImpl) Database(opts ...options.Option) (DatabaseProvider, erro
 
 // ObjectStore implements Container.
 func (c *containerImpl) ObjectStore(opts ...options.Option) (ObjectStoreProvider, error) {
-	c.onceComponents.Do(c.initializeComponents)
+	c.onceComponents.Do(func() {
+		c.initializeComponents()
+	})
 	if c.initErr != nil {
 		return nil, c.initErr
 	}
--- container/options.go
+++ container/options.go
@@ -1,2 +1,27 @@
 // Package container implements the functions, types, and interfaces for the module.
 package container
+
+import (
+	"github.com/origadmin/runtime/extension/optionutil"
+	"github.com/origadmin/runtime/interfaces/options"
+)
+
+type containerOptions struct {
+	componentFactories map[string]ComponentFactory
+}
+
+// WithComponentFactory is an option to register a ComponentFactory with the container.
+// It returns a function that applies the factory to the containerImpl.
+func WithComponentFactory(name string, factory ComponentFactory) options.Option {
+	return optionutil.Update(func(c *containerOptions) {
+		if c.componentFactories == nil {
+			c.componentFactories = make(map[string]ComponentFactory)
+		}
+		c.componentFactories[name] = factory
+	})
+}
+
+func ComponentFactoryFromOptions(opts ...options.Option) map[string]ComponentFactory {
+	o := optionutil.NewT[containerOptions](opts...)
+	return o.componentFactories
+}
